2ae9ed73de9d26cba4171bc67432aa3e655a08df
Shadow <william.shadow@outlook.com>
2020-12-13 09:49:14 +0000

    isis

--- .github/i18n.js
+++ .github/i18n.js
@@ -1,12 +1,20 @@
 const fs = require('fs');
 
 // list all files in the directory
-fs.readdir(__dirname + "../website/common/locales", (err, files) => {
+fs.readdir(__dirname + "/../website/common/locales", (err, locale) => {
     if (err) {
         throw err;
     }
 
     locale.forEach(locale => {
-        console.log(locale);
+      fs.readdir(__dirname + "/../website/common/locales/" + locale, (err, files) => {
+        if (err) {
+            throw err;
+        }
+    
+        files.forEach(file => {
+            console.log(file);
+        });
+    });
     });
 });

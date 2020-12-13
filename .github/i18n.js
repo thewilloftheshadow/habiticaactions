e7638ea1e98e67b5a0bbd39c110bb2647b22cd18
Shadow <william.shadow@outlook.com>
2020-12-13 09:14:03 +0000

    test

--- .github/i18n.js
+++ .github/i18n.js
@@ -1,1 +1,12 @@
-console.log("hi")
+const fs = require('fs');
+
+// list all files in the directory
+fs.readdir(__dirname + "/website/common/locales", (err, files) => {
+    if (err) {
+        throw err;
+    }
+
+    locale.forEach(locale => {
+        console.log(locale);
+    });
+});
